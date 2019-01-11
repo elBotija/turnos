af44b497fa4b268f42638892f9231d101828f96b
mmartinez <mmartinez@dosalcubo.com>
2019-01-11 17:12:28 -0300

    formulario de carga con validacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import AgregarCita from './components/AgregarCita'
 
 class App extends Component {
+
+    state = {
+        citas:[]
+    }
+
+    crearCita = (nuevaCita) => {
+        const citas = [...this.state.citas, nuevaCita]
+
+        console.log(citas)
+
+        this.setState({
+            citas
+        })
+    }
+
   render() {
     return (
-        <div className="App">
+        <div className="container">
             <Header
                 title="Administrador Turnos Veterinaria"
             />
+            <div className='row'>
+                <div className='col col-md-6'>
+                    <AgregarCita
+                        crearCita={this.crearCita}
+                    />
+                </div>
+            </div>
         </div>
     );
   }
