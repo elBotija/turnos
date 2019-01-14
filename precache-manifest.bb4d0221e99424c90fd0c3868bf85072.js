ecbd4be5edd90c8a91b6046e1f2406cba1705e98
elBotija <mauroenpixels@gmail.com>
2019-01-14 01:06:11 -0300

    Updates

---
+++ precache-manifest.bb4d0221e99424c90fd0c3868bf85072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d6d52241f50df87df26",
+    "url": "/turnos/static/js/runtime~main.6d6d5224.js"
+  },
+  {
+    "revision": "28a43875dd14b03e8ff4",
+    "url": "/turnos/static/js/main.28a43875.chunk.js"
+  },
+  {
+    "revision": "968940ace30198df9eca",
+    "url": "/turnos/static/js/1.968940ac.chunk.js"
+  },
+  {
+    "revision": "28a43875dd14b03e8ff4",
+    "url": "/turnos/static/css/main.e2536098.chunk.css"
+  },
+  {
+    "revision": "75cede3e5d3a5d961983db609c6958dc",
+    "url": "/turnos/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15ece31b0cc1916261ca5ce2e9eca34a.js"
+  "/turnos/precache-manifest.bb4d0221e99424c90fd0c3868bf85072.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/turnos/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
