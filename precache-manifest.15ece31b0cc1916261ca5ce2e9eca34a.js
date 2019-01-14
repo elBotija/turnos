d58661bd23d750b29ae7d3b819f6223fdcbd1bd9
elBotija <mauroenpixels@gmail.com>
2019-01-13 22:19:15 -0300

    Updates

---
+++ precache-manifest.15ece31b0cc1916261ca5ce2e9eca34a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8640d4219381ba42010e",
+    "url": "/static/js/main.8640d421.chunk.js"
+  },
+  {
+    "revision": "968940ace30198df9eca",
+    "url": "/static/js/1.968940ac.chunk.js"
+  },
+  {
+    "revision": "8640d4219381ba42010e",
+    "url": "/static/css/main.e2536098.chunk.css"
+  },
+  {
+    "revision": "9c70289a4c68d2ccc6b8932cc6537295",
+    "url": "/index.html"
+  }
+];
